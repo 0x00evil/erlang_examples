fbef520523cb456dae1e006a5faf49b3fb7ae8d9
gaoyuehua <gaoyuehua@goyoo.com>
2015-12-04 11:17:07 +0800

    first commit

---
+++ sockets/socket_example.erl
@@ -0,0 +1,18 @@
+-module(socket_example).
+-compile(export_all).
+-import(lists, [reverse/1]).
+nano_get_url() ->
+    nano_get_url("www.google.com"). %% 这里不能是http://www.baidu.com
+
+nano_get_url(Host) ->
+    {ok, Socket} = gen_tcp:connect(Host, 80, [binary, {packet, 0}]),
+    ok = gen_tcp:send(Socket, "GET / HTTP/1.0\r\n\r\n"),
+    receive_data(Socket, []).
+
+receive_data(Socket, SoFar) ->
+    receive
+        {tcp, Socket, Bin} ->
+            receive_data(Socket, [Bin | SoFar]);
+        {tcp_closed, Socket} ->
+            list_to_binary(reverse(SoFar))
+    end.
