e2d1dfddb59de323a4a412677a837cf5c9adb7f7
gaoyuehua <gaoyuehua@goyoo.com>
2016-01-25 17:05:27 +0800

    socket相关代码

--- sockets/socket_example.erl
+++ sockets/socket_example.erl
@@ -14,5 +14,68 @@ receive_data(Socket, SoFar) ->
         {tcp, Socket, Bin} ->
             receive_data(Socket, [Bin | SoFar]);
         {tcp_closed, Socket} ->
-            list_to_binary(reverse(SoFar))
+            io:format("~p~n", [list_to_binary(reverse(SoFar))])
     end.
+
+start_nano_server() ->
+    {ok, Listen} = gen_tcp:listen(2345, [binary, {packet, 4},
+                                         {reuseaddr, true},
+                                         {active, true}]),
+    {ok, Socket} = gen_tcp:accept(Listen),
+    gen_tcp:close(Listen),
+    loop(Socket).
+
+start_seq_server() ->
+    {ok, Listen} = gen_tcp:listen(2345, [binary, {packet, 4},
+                                         {reuseaddr, true},
+                                         {active, true}]),
+    seq_loop(Listen).
+
+seq_loop(Listen) ->
+    {ok, Socket} = gen_tcp:accept(Listen),
+    loop(Socket),
+    seq_loop(Listen).
+
+start_parallel_server() ->
+    {ok, Listen} = gen_tcp:listen(2345, [binary, {packet, 4},
+                                         {reuseaddr, true},
+                                         {active, true}]),
+    spawn(fun() -> par_connect(Listen) end).
+
+par_connect(Listen) ->
+    {ok, Socket} = gen_tcp:accept(Listen),
+    spawn(fun() -> par_connect(Listen) end),
+    loop(Socket).
+
+loop(Socket) ->
+    receive
+        {tcp, Socket, Bin} ->
+            io:format("Server received binary = ~p~n", [Bin]),
+            Str = binary_to_term(Bin),
+            io:format("Server unpacked ~p~n", [Str]),
+            Reply = string2value(Str),
+            io:format("\n"),
+            io:format("Server reply = ~p~n", [Reply]),
+            gen_tcp:send(Socket, term_to_binary(Reply)),
+            loop(Socket);
+        {tcp_closed, Socket} ->
+            io:format("Server socket closed~n")
+    end.
+
+nano_client_eval(Str) ->
+    {ok, Socket} = gen_tcp:connect("localhost", 2345, [binary, {packet, 4}]),
+    ok = gen_tcp:send(Socket, term_to_binary(Str)),
+    receive
+        {tcp, Socket, Bin} ->
+            io:format("Client received binary = ~p~n", [Bin]),
+            Val = binary_to_term(Bin),
+            io:format("Client result = ~p~n", [Val]),
+            gen_tcp:close(Socket)
+    end.
+
+string2value(Str) ->
+    {ok, Tokens, _} = erl_scan:string(Str ++ "."),
+    {ok, Exprs} = erl_parse:parse_exprs(Tokens),
+    Bindings = erl_eval:new_bindings(),
+    {value, Value, _} = erl_eval:exprs(Exprs, Bindings),
+    Value.
